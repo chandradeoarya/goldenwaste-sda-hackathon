7b37d152e67453e2284d5060e89fd713b84feab9
Chandradeo Arya <chandradeoaryaarya@gmail.com>
2025-02-01 18:12:50 +0530

    adding GA CICD bare metal tomcat

--- src/main/java/com/example/demo/GoldenWasteddApplication.java
+++ src/main/java/com/example/demo/GoldenWasteddApplication.java
@@ -7,6 +7,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GoldenWasteddApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.load();
+        System.setProperty("GOLDEN_DB_URL", dotenv.get("GOLDEN_DB_URL"));
+        System.setProperty("GOLDEN_DB_PORT", dotenv.get("GOLDEN_DB_PORT"));
+        System.setProperty("GOLDEN_DB_NAME", dotenv.get("GOLDEN_DB_NAME"));
+        System.setProperty("GOLDEN_DB_USERNAME", dotenv.get("GOLDEN_DB_USERNAME"));
+        System.setProperty("GOLDEN_DB_PASSWORD", dotenv.get("GOLDEN_DB_PASSWORD"));
+
+
 		SpringApplication.run(GoldenWasteddApplication.class, args);
 	}
 
